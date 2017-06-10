cc69321809861037084c46244c9b715e9545d6aa
unknown <sdak006@gmail.com>
2017-06-10 13:12:34 -0500

    first commit

---
+++ src/components/AlbumDetail.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
+import Button from './Button';
+
+const AlbumDetail = ({ album }) =>  {
+  //destructuring
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    headerContentStyle,
+    headerTextStyle,
+    thumbnailStyle, 
+    imageStyle,
+    thumbnailContainerStyle
+  } =  styles;
+
+  return (
+    <Card>
+      <CardSection>
+        <View style={thumbnailContainerStyle}>
+          <Image 
+            style={thumbnailStyle} 
+            source={{ uri: thumbnail_image }} 
+          />
+        </View>
+        <View style={headerContentStyle}>
+          <Text style={headerTextStyle}>{title}</Text>
+          <Text>{artist}</Text>
+        </View>
+      </CardSection>
+      
+      <CardSection>
+        <Image 
+            style={imageStyle}
+            source={{ uri: image }} 
+          />
+      </CardSection>
+
+      <CardSection>
+        <Button/>
+      </CardSection>
+    </Card>
+  );
+};
+
+const styles = {
+  imageStyle: {
+    //stretch image
+    height: 300,
+    flex: 1,
+    width: null
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  }
+};
+
+export default AlbumDetail
